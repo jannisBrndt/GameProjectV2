06cf1a0d50b5db3b90ffbb5d64031c78b469c082
jannisBrndt <100727375+jannisBrndt@users.noreply.github.com>
2024-12-05 02:28:06 +0100

    Delete build directory

--- build/CMakeFiles/GameTests.dir/src/Game/Components/Stats.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/GameTests.dir/src/Game/Components/Stats.cpp.o: \
-  /Users/jannis/Desktop/GameProjectV2/src/Game/Components/Stats.cpp \
-  /Users/jannis/Desktop/GameProjectV2/src/Game/Components/../../../include/Game/Components/Stats.h
--- build/CMakeFiles/GameTests.dir/tests/GameTestIntegration.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/GameTests.dir/tests/GameTestIntegration.cpp.o: \
-  /Users/jannis/Desktop/GameProjectV2/tests/GameTestIntegration.cpp
